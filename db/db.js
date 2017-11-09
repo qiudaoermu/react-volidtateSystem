3363a9a8acecfe94fcb02cc26e66fa9907792467
qiudaoermu <1540274138@qq.com>
2017-11-09 22:55:12 +0800

    no message

---
+++ db/db.js
@@ -0,0 +1,962 @@
+/* 1 */
+{
+    "_id" : ObjectId("598c6d0ffdcf190b45767c0c"),
+    "characterId" : "91761836",
+    "name" : "Furos Anninen",
+    "race" : "Caldari",
+    "bloodline" : "Deteis",
+    "gender" : "Female",
+    "random" : [ 
+        0.618795348333495, 
+        0
+    ],
+    "voted" : false,
+    "reports" : 1,
+    "losses" : 0,
+    "wins" : 0,
+    "__v" : 0
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("51b4b65c4901c2ea77000075"),
+    "__v" : 870,
+    "bloodline" : "Civire",
+    "characterId" : "1702379218",
+    "gender" : "Male",
+    "losses" : 455,
+    "name" : "Nickomite",
+    "race" : "Caldari",
+    "random" : [ 
+        0.885926224989817, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 405
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("51b4b4474901c2ea7700000c"),
+    "__v" : 872,
+    "bloodline" : "Ni-Kunni",
+    "characterId" : "374969003",
+    "gender" : "Female",
+    "losses" : 480,
+    "name" : "Sharon Maris",
+    "race" : "Amarr",
+    "random" : [ 
+        0.00283340853638947, 
+        0
+    ],
+    "reports" : 4,
+    "voted" : false,
+    "wins" : 380,
+    "wrongGender" : false
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("51b4b6784901c2ea7700007c"),
+    "__v" : 872,
+    "bloodline" : "Vherokior",
+    "characterId" : "1463317054",
+    "gender" : "Female",
+    "losses" : 548,
+    "name" : "Moca Jade",
+    "race" : "Minmatar",
+    "random" : [ 
+        0.904437087010592, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 312
+}
+
+/* 5 */
+{
+    "_id" : ObjectId("51b4b8514901c2ea770000dc"),
+    "__v" : 871,
+    "bloodline" : "Khanid",
+    "characterId" : "90652520",
+    "gender" : "Female",
+    "losses" : 412,
+    "name" : "Cammy's Hope",
+    "race" : "Amarr",
+    "random" : [ 
+        0.0492462383117527, 
+        0
+    ],
+    "reports" : 1,
+    "voted" : false,
+    "wins" : 449
+}
+
+/* 6 */
+{
+    "_id" : ObjectId("51b4b8b24901c2ea770000f2"),
+    "__v" : 870,
+    "bloodline" : "Amarr",
+    "characterId" : "1311254993",
+    "gender" : "Male",
+    "losses" : 356,
+    "name" : "luZk",
+    "race" : "Amarr",
+    "random" : [ 
+        0.048831932945177, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 505
+}
+
+/* 7 */
+{
+    "_id" : ObjectId("51b4b96a4901c2ea7700011b"),
+    "__v" : 871,
+    "bloodline" : "Amarr",
+    "characterId" : "91851811",
+    "gender" : "Female",
+    "losses" : 485,
+    "name" : "Amalurra Starr",
+    "race" : "Amarr",
+    "random" : [ 
+        0.19632158218883, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 375
+}
+
+/* 8 */
+{
+    "_id" : ObjectId("51b4b9ee4901c2ea77000141"),
+    "__v" : 872,
+    "bloodline" : "Achura",
+    "characterId" : "636301583",
+    "gender" : "Male",
+    "losses" : 356,
+    "name" : "industrial oblitorator",
+    "race" : "Caldari",
+    "random" : [ 
+        0.465903993928805, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 505
+}
+
+/* 9 */
+{
+    "_id" : ObjectId("51b4ba1c4901c2ea77000151"),
+    "__v" : 869,
+    "bloodline" : "Amarr",
+    "characterId" : "90532963",
+    "gender" : "Male",
+    "losses" : 399,
+    "name" : "Alucard Shadows",
+    "race" : "Amarr",
+    "random" : [ 
+        0.879515932640061, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 461
+}
+
+/* 10 */
+{
+    "_id" : ObjectId("51b4f47500808a4c5c00003c"),
+    "__v" : 871,
+    "bloodline" : "Civire",
+    "characterId" : "1362870121",
+    "gender" : "Male",
+    "losses" : 417,
+    "name" : "Deock",
+    "race" : "Caldari",
+    "random" : [ 
+        0.975329605862498, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 443
+}
+
+/* 11 */
+{
+    "_id" : ObjectId("51b4f8e000808a4c5c000111"),
+    "__v" : 873,
+    "bloodline" : "Sebiestor",
+    "characterId" : "159345822",
+    "gender" : "Male",
+    "losses" : 445,
+    "name" : "Epsilon",
+    "race" : "Minmatar",
+    "random" : [ 
+        0.446079722838476, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 415,
+    "wrongGender" : false
+}
+
+/* 12 */
+{
+    "_id" : ObjectId("51b4fb0700808a4c5c00017c"),
+    "__v" : 870,
+    "bloodline" : "Deteis",
+    "characterId" : "178807923",
+    "gender" : "Female",
+    "losses" : 384,
+    "name" : "Akima",
+    "race" : "Caldari",
+    "random" : [ 
+        0.170397930312902, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 477,
+    "wrongGender" : false
+}
+
+/* 13 */
+{
+    "_id" : ObjectId("51b5029800808a4c5c0001a2"),
+    "__v" : 870,
+    "bloodline" : "Deteis",
+    "characterId" : "163273173",
+    "gender" : "Female",
+    "losses" : 283,
+    "name" : "Jasmine",
+    "race" : "Caldari",
+    "random" : [ 
+        0.502544865012169, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 578
+}
+
+/* 14 */
+{
+    "_id" : ObjectId("51b502cf00808a4c5c0001a6"),
+    "__v" : 871,
+    "bloodline" : "Intaki",
+    "characterId" : "899572606",
+    "gender" : "Male",
+    "losses" : 346,
+    "name" : "Lavititcus",
+    "race" : "Gallente",
+    "random" : [ 
+        0.888513174373657, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 514
+}
+
+/* 15 */
+{
+    "_id" : ObjectId("51b5043000808a4c5c0001c9"),
+    "__v" : 871,
+    "bloodline" : "Gallente",
+    "characterId" : "732758278",
+    "gender" : "Male",
+    "losses" : 442,
+    "name" : "Lhaim",
+    "race" : "Gallente",
+    "random" : [ 
+        0.250278607010841, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 418
+}
+
+/* 16 */
+{
+    "_id" : ObjectId("51b514d1142fd9ff3500000e"),
+    "__v" : 871,
+    "bloodline" : "Intaki",
+    "characterId" : "916132673",
+    "gender" : "Female",
+    "losses" : 376,
+    "name" : "Agent Akari",
+    "race" : "Gallente",
+    "random" : [ 
+        0.753181697800756, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 485,
+    "wrongGender" : false
+}
+
+/* 17 */
+{
+    "_id" : ObjectId("51b5168f142fd9ff3500005c"),
+    "__v" : 873,
+    "bloodline" : "Sebiestor",
+    "characterId" : "1619037491",
+    "gender" : "Female",
+    "losses" : 352,
+    "name" : "Number 874",
+    "race" : "Minmatar",
+    "random" : [ 
+        0.907149645732716, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 509
+}
+
+/* 18 */
+{
+    "_id" : ObjectId("51b516b3142fd9ff35000063"),
+    "__v" : 873,
+    "bloodline" : "Gallente",
+    "characterId" : "786605657",
+    "gender" : "Female",
+    "losses" : 418,
+    "name" : "Elnora Lynes",
+    "race" : "Gallente",
+    "random" : [ 
+        0.274409007746726, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 443
+}
+
+/* 19 */
+{
+    "_id" : ObjectId("51b51789142fd9ff35000086"),
+    "__v" : 870,
+    "bloodline" : "Jin-Mei",
+    "characterId" : "92597003",
+    "gender" : "Female",
+    "losses" : 436,
+    "name" : "Pompeia Rufus",
+    "race" : "Gallente",
+    "random" : [ 
+        0.480802001431584, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 425,
+    "wrongGender" : false
+}
+
+/* 20 */
+{
+    "_id" : ObjectId("51b51f7fb08489543b00001b"),
+    "__v" : 872,
+    "bloodline" : "Civire",
+    "characterId" : "91768499",
+    "gender" : "Male",
+    "losses" : 444,
+    "name" : "Fufai Ter",
+    "race" : "Caldari",
+    "random" : [ 
+        0.807189219398424, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 416
+}
+
+/* 21 */
+{
+    "_id" : ObjectId("51b51fa7b08489543b000025"),
+    "__v" : 872,
+    "bloodline" : "Achura",
+    "characterId" : "820303498",
+    "gender" : "Female",
+    "losses" : 229,
+    "name" : "Elise Randolph",
+    "race" : "Caldari",
+    "random" : [ 
+        0.813817212120932, 
+        0
+    ],
+    "reports" : 1,
+    "voted" : true,
+    "wins" : 632
+}
+
+/* 22 */
+{
+    "_id" : ObjectId("51b52072b08489543b000057"),
+    "__v" : 870,
+    "bloodline" : "Deteis",
+    "characterId" : "1428616504",
+    "gender" : "Male",
+    "losses" : 399,
+    "name" : "Gravemind GER",
+    "race" : "Caldari",
+    "random" : [ 
+        0.688452382339165, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 462
+}
+
+/* 23 */
+{
+    "_id" : ObjectId("51b520c9b08489543b00006b"),
+    "__v" : 871,
+    "bloodline" : "Amarr",
+    "characterId" : "91957717",
+    "gender" : "Male",
+    "losses" : 404,
+    "name" : "CALIBRE AK47",
+    "race" : "Amarr",
+    "random" : [ 
+        0.505737693747506, 
+        0
+    ],
+    "reports" : 4,
+    "voted" : false,
+    "wins" : 457,
+    "wrongGender" : false
+}
+
+/* 24 */
+{
+    "_id" : ObjectId("51b520e8b08489543b000073"),
+    "__v" : 871,
+    "bloodline" : "Amarr",
+    "characterId" : "922355212",
+    "gender" : "Male",
+    "losses" : 384,
+    "name" : "Vile rat",
+    "race" : "Amarr",
+    "random" : [ 
+        0.0825005709193647, 
+        0
+    ],
+    "reports" : 1,
+    "voted" : false,
+    "wins" : 477
+}
+
+/* 25 */
+{
+    "_id" : ObjectId("51b60e9ef3331c3732000004"),
+    "__v" : 872,
+    "bloodline" : "Deteis",
+    "characterId" : "92778196",
+    "gender" : "Male",
+    "losses" : 426,
+    "name" : "Dr Trolo",
+    "race" : "Caldari",
+    "random" : [ 
+        0.666554251685739, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 435
+}
+
+/* 26 */
+{
+    "_id" : ObjectId("51b61975f3331c3732000015"),
+    "__v" : 874,
+    "bloodline" : "Sebiestor",
+    "characterId" : "91112888",
+    "gender" : "Female",
+    "losses" : 406,
+    "name" : "Cassadine Reynolds",
+    "race" : "Minmatar",
+    "random" : [ 
+        0.0818960722535849, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 456
+}
+
+/* 27 */
+{
+    "_id" : ObjectId("51b61982f3331c3732000016"),
+    "__v" : 871,
+    "bloodline" : "Civire",
+    "characterId" : "91271646",
+    "gender" : "Female",
+    "losses" : 320,
+    "name" : "Sheryl Reynolds",
+    "race" : "Caldari",
+    "random" : [ 
+        0.325948115205392, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 541,
+    "wrongGender" : false
+}
+
+/* 28 */
+{
+    "_id" : ObjectId("51b62bcbf3331c373200002f"),
+    "__v" : 871,
+    "bloodline" : "Intaki",
+    "characterId" : "1035218104",
+    "gender" : "Female",
+    "losses" : 283,
+    "name" : "War Kitten",
+    "race" : "Gallente",
+    "random" : [ 
+        0.789981166599318, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 577
+}
+
+/* 29 */
+{
+    "_id" : ObjectId("51b64977f3331c3732000059"),
+    "__v" : 872,
+    "bloodline" : "Ni-Kunni",
+    "characterId" : "93443362",
+    "gender" : "Male",
+    "losses" : 372,
+    "name" : "Michael Elieser",
+    "race" : "Amarr",
+    "random" : [ 
+        0.869400080991909, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 489
+}
+
+/* 30 */
+{
+    "_id" : ObjectId("51b65873f3331c3732000067"),
+    "__v" : 871,
+    "bloodline" : "Amarr",
+    "characterId" : "91624595",
+    "gender" : "Female",
+    "losses" : 546,
+    "name" : "Anna Reynolds",
+    "race" : "Amarr",
+    "random" : [ 
+        0.318630314664915, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 315,
+    "wrongGender" : false
+}
+
+/* 31 */
+{
+    "_id" : ObjectId("51b6dc03548ca3ce14000028"),
+    "__v" : 871,
+    "bloodline" : "Gallente",
+    "characterId" : "1090717149",
+    "gender" : "Male",
+    "losses" : 412,
+    "name" : "Armored Phoenix",
+    "race" : "Gallente",
+    "random" : [ 
+        0.743127014953643, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 449,
+    "wrongGender" : false
+}
+
+/* 32 */
+{
+    "_id" : ObjectId("51b6ebf4548ca3ce14000033"),
+    "__v" : 870,
+    "bloodline" : "Civire",
+    "characterId" : "149891156",
+    "gender" : "Female",
+    "losses" : 283,
+    "name" : "Kelera",
+    "race" : "Caldari",
+    "random" : [ 
+        0.511301711201668, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 578
+}
+
+/* 33 */
+{
+    "_id" : ObjectId("51b7387be2a7439721000008"),
+    "__v" : 872,
+    "bloodline" : "Brutor",
+    "characterId" : "645777543",
+    "gender" : "Female",
+    "losses" : 282,
+    "name" : "Zoe Stormrunner",
+    "race" : "Minmatar",
+    "random" : [ 
+        0.946544459089637, 
+        0
+    ],
+    "reports" : 1,
+    "voted" : false,
+    "wins" : 580
+}
+
+/* 34 */
+{
+    "_id" : ObjectId("51b7388ae2a7439721000009"),
+    "__v" : 870,
+    "bloodline" : "Khanid",
+    "characterId" : "1197716395",
+    "gender" : "Male",
+    "losses" : 437,
+    "name" : "Garruda",
+    "race" : "Amarr",
+    "random" : [ 
+        0.824073048075661, 
+        0
+    ],
+    "reports" : 1,
+    "voted" : false,
+    "wins" : 424
+}
+
+/* 35 */
+{
+    "_id" : ObjectId("51b76a33e2a743972100002c"),
+    "__v" : 871,
+    "bloodline" : "Jin-Mei",
+    "characterId" : "91622714",
+    "gender" : "Female",
+    "losses" : 199,
+    "name" : "Ila Dace",
+    "race" : "Gallente",
+    "random" : [ 
+        0.283305368386209, 
+        0
+    ],
+    "reports" : 2,
+    "voted" : false,
+    "wins" : 663,
+    "wrongGender" : true
+}
+
+/* 36 */
+{
+    "_id" : ObjectId("51b76b2be2a743972100002e"),
+    "__v" : 871,
+    "bloodline" : "Intaki",
+    "characterId" : "92767098",
+    "gender" : "Female",
+    "losses" : 306,
+    "name" : "Riall",
+    "race" : "Gallente",
+    "random" : [ 
+        0.643980412511155, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 555,
+    "wrongGender" : false
+}
+
+/* 37 */
+{
+    "_id" : ObjectId("51b79fbde2a7439721000049"),
+    "__v" : 871,
+    "bloodline" : "Ni-Kunni",
+    "characterId" : "91193034",
+    "gender" : "Female",
+    "losses" : 305,
+    "name" : "Keily Shane",
+    "race" : "Amarr",
+    "random" : [ 
+        0.855277572292835, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 556
+}
+
+/* 38 */
+{
+    "_id" : ObjectId("51b83f56e2a7439721000063"),
+    "__v" : 870,
+    "bloodline" : "Jin-Mei",
+    "characterId" : "93451766",
+    "gender" : "Female",
+    "losses" : 324,
+    "name" : "Peaches Anne Creme",
+    "race" : "Gallente",
+    "random" : [ 
+        0.147630285006016, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 536
+}
+
+/* 39 */
+{
+    "_id" : ObjectId("51b85c5de2a743972100006a"),
+    "__v" : 870,
+    "bloodline" : "Brutor",
+    "characterId" : "93132227",
+    "gender" : "Female",
+    "losses" : 289,
+    "name" : "Dalisay",
+    "race" : "Minmatar",
+    "random" : [ 
+        0.0962099411990494, 
+        0
+    ],
+    "reports" : 1,
+    "voted" : false,
+    "wins" : 571
+}
+
+/* 40 */
+{
+    "_id" : ObjectId("51b87376e2a743972100007a"),
+    "__v" : 875,
+    "bloodline" : "Khanid",
+    "characterId" : "1194858977",
+    "gender" : "Female",
+    "losses" : 283,
+    "name" : "Aenrea",
+    "race" : "Amarr",
+    "random" : [ 
+        0.243598745204508, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 579
+}
+
+/* 41 */
+{
+    "_id" : ObjectId("51b8a094b01dd73d28000005"),
+    "__v" : 871,
+    "bloodline" : "Amarr",
+    "characterId" : "91710448",
+    "gender" : "Female",
+    "losses" : 529,
+    "name" : "Soap Bubble",
+    "race" : "Amarr",
+    "random" : [ 
+        0.102561728330329, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 333,
+    "wrongGender" : false
+}
+
+/* 42 */
+{
+    "_id" : ObjectId("51b8aadfb01dd73d2800000e"),
+    "__v" : 872,
+    "bloodline" : "Achura",
+    "characterId" : "93369732",
+    "gender" : "Female",
+    "losses" : 286,
+    "name" : "Yasha Okami",
+    "race" : "Caldari",
+    "random" : [ 
+        0.962226454401389, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 575
+}
+
+/* 43 */
+{
+    "_id" : ObjectId("51b8f626b01dd73d2800004a"),
+    "__v" : 871,
+    "bloodline" : "Deteis",
+    "characterId" : "90756219",
+    "gender" : "Female",
+    "losses" : 401,
+    "name" : "Lux Marquis",
+    "race" : "Caldari",
+    "random" : [ 
+        0.91296367533505, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 460
+}
+
+/* 44 */
+{
+    "_id" : ObjectId("51b908f7b01dd73d2800005b"),
+    "__v" : 871,
+    "bloodline" : "Sebiestor",
+    "characterId" : "1101959335",
+    "gender" : "Male",
+    "losses" : 369,
+    "name" : "Jakob Voraine",
+    "race" : "Minmatar",
+    "random" : [ 
+        0.0818797843530774, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 492
+}
+
+/* 45 */
+{
+    "_id" : ObjectId("51b9111cb01dd73d28000064"),
+    "__v" : 870,
+    "bloodline" : "Ni-Kunni",
+    "characterId" : "1908388592",
+    "gender" : "Female",
+    "losses" : 463,
+    "name" : "Tabbie Te'Jir",
+    "race" : "Amarr",
+    "random" : [ 
+        0.30926204379648, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 396,
+    "wrongGender" : false
+}
+
+/* 46 */
+{
+    "_id" : ObjectId("51b91b29b01dd73d28000068"),
+    "__v" : 870,
+    "bloodline" : "Brutor",
+    "characterId" : "361664294",
+    "gender" : "Male",
+    "losses" : 422,
+    "name" : "Talak Relketh",
+    "race" : "Minmatar",
+    "random" : [ 
+        0.484402967616916, 
+        0
+    ],
+    "reports" : 1,
+    "voted" : false,
+    "wins" : 439
+}
+
+/* 47 */
+{
+    "_id" : ObjectId("51b921d5b01dd73d28000069"),
+    "__v" : 871,
+    "bloodline" : "Sebiestor",
+    "characterId" : "90813229",
+    "gender" : "Female",
+    "losses" : 393,
+    "name" : "Luv Rocket",
+    "race" : "Minmatar",
+    "random" : [ 
+        0.868010357255116, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 468
+}
+
+/* 48 */
+{
+    "_id" : ObjectId("51b92b41b01dd73d2800006d"),
+    "__v" : 870,
+    "bloodline" : "Civire",
+    "characterId" : "93071998",
+    "gender" : "Male",
+    "losses" : 392,
+    "name" : "Rokkton",
+    "race" : "Caldari",
+    "random" : [ 
+        0.0459311220329255, 
+        0
+    ],
+    "reports" : 1,
+    "voted" : false,
+    "wins" : 469
+}
+
+/* 49 */
+{
+    "_id" : ObjectId("51b9355eb01dd73d28000071"),
+    "__v" : 870,
+    "bloodline" : "Civire",
+    "characterId" : "681548744",
+    "gender" : "Female",
+    "losses" : 352,
+    "name" : "Marlona Sky",
+    "race" : "Caldari",
+    "random" : [ 
+        0.67494896473363, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : false,
+    "wins" : 509
+}
+
+/* 50 */
+{
+    "_id" : ObjectId("51b93d38c9cbe80f3c00000f"),
+    "__v" : 869,
+    "bloodline" : "Jin-Mei",
+    "characterId" : "92136708",
+    "gender" : "Female",
+    "losses" : 335,
+    "name" : "Rin Nagamori",
+    "race" : "Gallente",
+    "random" : [ 
+        0.627688563408696, 
+        0
+    ],
+    "reports" : 0.0,
+    "voted" : true,
+    "wins" : 525
+}
